d884a499c54ebc479e46988fdedecb170dc58226
Ish Thumber <80597098+IshThumber@users.noreply.github.com>
2022-12-03 14:55:52 +0530

    404 ejs

--- routes/shop.js
+++ routes/shop.js
@@ -4,8 +4,6 @@ const router = express.Router();
 
 const productsController = require('../controllers/products');
 
-const adminData = require('./admin');
-
 router.get('/', productsController.getProducts);
 
 module.exports = router;
